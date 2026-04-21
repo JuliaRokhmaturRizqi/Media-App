8d2a156bf83d84c8138f50a36c31a17eb2c75b7c
Julia Rokhmatur Rizqi <juliarohmaturrizqi@gmail.com>
2026-04-22 04:26:00 +0700

    mengisi models

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kategori extends Model
 {
     //
+    use HasFactory;
+
+    protected $table = 'kategori';
+    protected $primaryKey = 'kategori_id';
+    protected $fillable = ['nama_kategori'];
 }
--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
     //
+    use HasFactory;
+    protected $table = 'media';
+    protected $primaryKey = 'media_id';
+
+    //Semua laci yang boleh diisi harus didaftarkan di sini
+    protected $fillable = [
+        'mahasiswa_id',
+        'kategori_id',
+        'judul',
+        'deskripsi',
+        'judul_penelitian',
+        'tahun_terbit',
+        'link_media',
+        'gambar_cover'
+    ];
 }
